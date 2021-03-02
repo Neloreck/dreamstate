0de6f3c715b8e88d11fc53d676ba1ae8791de168
neloreck <neloreck@gmail.com>
2021-03-02 23:22:00 +0200

    'withProvision' hoc typing update.

--- src/dreamstate/core/provision/withProvision.ts
+++ src/dreamstate/core/provision/withProvision.ts
@@ -1,6 +1,7 @@
 import { Provide } from "@/dreamstate/core/provision/Provide";
+import { TDreamstateService } from "@/dreamstate/types/";
 
 /**
  * HOC alias for @Provide.
  */
-export const withProvision = Provide;
+export const withProvision = Provide as <T>(sources: Array<TDreamstateService>) => ((component: T) => T);
