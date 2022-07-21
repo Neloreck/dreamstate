69bfdcaa7d7486771cd41687fb1226c792f01af5
neloreck <neloreck@gmail.com>
2022-07-22 02:38:37 +0300

    Separate method for error code mapping to message.
    Signed-off-by: neloreck <neloreck@gmail.com>

--- src/dreamstate/core/error/DreamstateError.ts
+++ src/dreamstate/core/error/DreamstateError.ts
@@ -1,3 +1,4 @@
+import { mapDreamstateErrorMessage } from "@/dreamstate/core/error/mapErrorMessage";
 import { EDreamstateErrorCode } from "@/dreamstate/types";
 
 /**
@@ -20,47 +21,11 @@ export class DreamstateError extends Error {
    */
   public readonly message: string;
 
-  public readonly detail?: string;
-
   public constructor(code: EDreamstateErrorCode = EDreamstateErrorCode.UNEXPECTED_ERROR, detail?: string) {
     super();
 
     this.code = code;
-    this.detail = detail;
-    this.message = this.getMessage();
-  }
-
-  /**
-   * Get message based on current error code.
-   */
-  public getMessage(): string {
-    switch (this.code) {
-      case EDreamstateErrorCode.INSTANCE_DISPOSED:
-        return "Disposed instances are not supposed to access scope.";
-      case EDreamstateErrorCode.OUT_OF_SCOPE:
-        return "Instance is out of scope, make sure it is created or mocked correctly.";
-      case EDreamstateErrorCode.INCORRECT_PARAMETER:
-        return `Incorrect parameter supplied.${this.detail ? ` ${this.detail}` : ""}`;
-      case EDreamstateErrorCode.INCORRECT_SIGNAL_TYPE:
-        return `Unexpected signal type provided, expected symbol, string or number.${
-          this.detail ? ` Got: '${this.detail}' instead.` : ""
-        }`;
-      case EDreamstateErrorCode.INCORRECT_SIGNAL_LISTENER:
-        return `Signal listener must be function, '${this.detail}' provided.`;
-      case EDreamstateErrorCode.INCORRECT_QUERY_TYPE:
-        return `Unexpected query type provided, expected symbol, string or number. Got: '${this.detail}' instead.`;
-      case EDreamstateErrorCode.INCORRECT_QUERY_PROVIDER:
-        return `Query provider must be factory function, '${this.detail}' provided.`;
-      case EDreamstateErrorCode.TARGET_CONTEXT_MANAGER_EXPECTED:
-        return `Cannot perform action, class extending ContextManager is expected.${
-          this.detail ? ` ${this.detail}` : ""
-        }`;
-      case EDreamstateErrorCode.RESTRICTED_OPERATION:
-        return `Operation is restricted.${this.detail ? ` ${this.detail}` : ""}`;
-      case EDreamstateErrorCode.UNEXPECTED_ERROR:
-      default:
-        return "Unexpected dreamstate error.";
-    }
+    this.message = mapDreamstateErrorMessage(code, detail);
   }
 
 }
---
+++ src/dreamstate/core/error/mapErrorMessage.ts
@@ -0,0 +1,29 @@
+import { EDreamstateErrorCode } from "@/dreamstate/types";
+
+export function mapDreamstateErrorMessage(code: EDreamstateErrorCode, detail?: string): string {
+  switch (code) {
+    case EDreamstateErrorCode.INSTANCE_DISPOSED:
+      return "Disposed instances are not supposed to access scope.";
+    case EDreamstateErrorCode.OUT_OF_SCOPE:
+      return "Instance is out of scope, make sure it is created or mocked correctly.";
+    case EDreamstateErrorCode.INCORRECT_PARAMETER:
+      return `Incorrect parameter supplied.${detail ? ` ${detail}` : ""}`;
+    case EDreamstateErrorCode.INCORRECT_SIGNAL_TYPE:
+      return `Unexpected signal type provided, expected symbol, string or number.${
+        detail ? ` Got: '${detail}' instead.` : ""
+      }`;
+    case EDreamstateErrorCode.INCORRECT_SIGNAL_LISTENER:
+      return `Signal listener must be function, '${detail}' provided.`;
+    case EDreamstateErrorCode.INCORRECT_QUERY_TYPE:
+      return `Unexpected query type provided, expected symbol, string or number. Got: '${detail}' instead.`;
+    case EDreamstateErrorCode.INCORRECT_QUERY_PROVIDER:
+      return `Query provider must be factory function, '${detail}' provided.`;
+    case EDreamstateErrorCode.TARGET_CONTEXT_MANAGER_EXPECTED:
+      return `Cannot perform action, class extending ContextManager is expected.${detail ? ` ${detail}` : ""}`;
+    case EDreamstateErrorCode.RESTRICTED_OPERATION:
+      return `Operation is restricted.${detail ? ` ${detail}` : ""}`;
+    case EDreamstateErrorCode.UNEXPECTED_ERROR:
+    default:
+      return "Unexpected dreamstate error.";
+  }
+}
