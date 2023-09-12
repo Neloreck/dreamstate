52982788b2013cdad32ccc5242364598a1232406
Neloreck <Neloreck@gmail.com>
2023-09-12 21:49:50 +0300

    Fix linting step.

--- examples/signal_application/src/index.tsx
+++ examples/signal_application/src/index.tsx
@@ -1,14 +1,15 @@
-import React, { StrictMode } from 'react';
-import { Root, createRoot } from 'react-dom/client';
-import { Application } from './Application';
+import React, { StrictMode } from "react";
+import { Root, createRoot } from "react-dom/client";
+
+import { Application } from "./Application";
 
 const root: Root = createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 
 root.render(
   <StrictMode>
-    <Application />
+    <Application/>
   </StrictMode>
 );
 
